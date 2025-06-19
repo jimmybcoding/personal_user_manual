e938a0f996c9a8da9072fca2f04cf9858c3cf078
James Barbour <jameskbarbour1@gmail.com>
2025-06-19 12:29:56 -0400

    Remove apos

--- src/app/components/Motivators/DealBreakers/motivators_deal_breakers.jsx
+++ src/app/components/Motivators/DealBreakers/motivators_deal_breakers.jsx
@@ -8,7 +8,7 @@ const Motivators_Deal_Breakers = () => {
             </div>
             <div className="lg:w-1/2 sm:w-full border-2 border-yellow-500 rounded-xl my-6 p-6 bg-yellow-300">
                  <h2 className="font-bold text-xl border-b-yellow-500 border-b-2 mb-4 w-fit">Deal Breakers</h2>
-                 <p className="mb-6">Dealbreakers include people who bad mouth their partners. It instantly makes me we wary of you and think that we aren't like minded people. Also, co-workers who don't pull their own weight but talk as if they do. Any sort of group team building or work diner outside of work hours I will never attend. Absolute biggest deal breaker of all time is mix and mingle, social networking type work functions with people who I do not know. I just don't go to these anymore.</p>        
+                 <p className="mb-6">Dealbreakers include people who bad mouth their partners. It instantly makes me we wary of you and think that we are not like minded people. Also, co-workers who do not pull their own weight but talk as if they do. Any sort of group team building or work diner outside of work hours I will never attend. Absolute biggest deal breaker of all time is mix and mingle, social networking type work functions with people who I do not know. I just do not go to these anymore.</p>        
             </div>
         </div> 
     )
